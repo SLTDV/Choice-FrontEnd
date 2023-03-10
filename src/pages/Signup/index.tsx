7fc8fe6f5447d8fc40da18667c875f755124ba70
minjekang <kangminje777@gmail.com>
2023-03-10 16:06:40 +0900

    sinup page logo

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -2,7 +2,16 @@ import React from 'react';
 import * as S from './style';
 import * as I from '../../asset/svg';
 const Signup = () => {
-  return <div></div>;
+  return (
+    <S.Layout>
+      <S.LogoLayout>
+        <S.LogoWrap>
+          <I.Logo />
+          <p>선택과 고민을 한 번에</p>
+        </S.LogoWrap>
+      </S.LogoLayout>
+    </S.Layout>
+  );
 };
 
 export default Signup;
--- src/pages/Signup/style.ts
+++ src/pages/Signup/style.ts
@@ -1,3 +1,39 @@
+import { keyframes } from '@emotion/react';
 import styled from '@emotion/styled';
 
-export const P = styled.p``;
+const fadeIn = keyframes`
+    0%{
+        opacity: 0;
+        margin-bottom: -10px;
+    }
+    100%{
+        opacity: 1;
+        margin-bottom: 0;
+    }
+`;
+
+export const Layout = styled.section`
+  width: 100%;
+  height: 100%;
+  display: flex;
+`;
+
+export const LogoLayout = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  padding: 0 18.23%;
+`;
+
+export const LogoWrap = styled.div`
+  text-align: right;
+  svg {
+    animation: ${fadeIn} 0.5s;
+  }
+  p {
+    margin: 0;
+    color: #6d6d6d;
+    animation: ${fadeIn} 1.5s;
+  }
+`;
