6db2a7f40537054b851607830930c5ea53c37c3b
minjekang <kangminje777@gmail.com>
2023-03-10 15:01:13 +0900

    remove legacy

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -1,13 +1,8 @@
-import * as S from './style';
 import React from 'react';
-
+import * as S from './style';
+import * as I from '../../asset/svg';
 const Signup = () => {
-  return (
-    <div>
-      <S.P>LOG IN</S.P>
-      <S.P>선택의 고민을 한번에</S.P>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Signup;
