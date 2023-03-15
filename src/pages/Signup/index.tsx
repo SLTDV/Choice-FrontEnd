e951b0f6dc42b005caaf2823592efe05cbc19a32
minjekang <kangminje777@gmail.com>
2023-03-15 17:31:03 +0900

    add: email 정규식

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -74,6 +74,11 @@ const Signup = () => {
               <S.Input
                 {...register('email', {
                   required: '이메일을 입력해주세요.',
+                  pattern: {
+                    message: '올바른 이메일을 입력해주세요',
+                    value:
+                      /[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/,
+                  },
                 })}
                 type='text'
                 isError={isError}
