4a8ab9e6eb4ef35c263afee963506acda2d42c7f
minjekang <kangminje777@gmail.com>
2023-03-17 13:44:21 +0900

    add: signin form

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import * as S from './style';
 import * as I from '../../asset/svg';
+import { Link } from 'react-router-dom';
 
 const Signin = () => {
   return (
@@ -11,6 +12,18 @@ const Signin = () => {
           <p>선택의 고민을 한 번에</p>
         </S.LogoWrap>
       </S.LogoLayout>
+      <S.SigninLayout>
+        <S.SigninForm>
+          <h1>LOG IN</h1>
+          <h3>Choice 회원로그인</h3>
+          <S.Input />
+          <S.Input />
+          <S.Button>로그인</S.Button>
+          <S.GoSignup>
+            <Link to='/signup'>회원가입</Link>
+          </S.GoSignup>
+        </S.SigninForm>
+      </S.SigninLayout>
     </S.Layout>
   );
 };
--- src/pages/Signin/style.ts
+++ src/pages/Signin/style.ts
@@ -40,3 +40,68 @@ export const LogoWrap = styled.div`
     font-size: 2rem;
   }
 `;
+
+export const SigninLayout = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
+
+export const SigninForm = styled.form`
+  text-align: center;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  h1 {
+    font-weight: 200;
+    font-size: 5rem;
+    font-family: AppleSDGothicNeoUL;
+  }
+  h3 {
+    font-weight: 300;
+    font-size: 1.4rem;
+    line-height: 17px;
+    margin: -1.2rem 0 5rem 0;
+  }
+`;
+
+export const Input = styled.input`
+  width: 57rem;
+  height: 7.5rem;
+  border: 2px solid #737373;
+  border-radius: 2rem;
+  margin-bottom: 2rem;
+  font-size: 1.8rem;
+  padding-left: 2rem;
+  &:focus {
+    background: #f3f3f3;
+    outline: none;
+    border: 2px solid #000000;
+  }
+`;
+
+export const Button = styled.button`
+  width: 60rem;
+  height: 7.5rem;
+  background-color: #000000;
+  border-radius: 2rem;
+  color: #ffffff;
+  font-size: 2.4rem;
+  margin-top: 6rem;
+  cursor: pointer;
+`;
+
+export const GoSignup = styled.div`
+  margin-top: 2.8rem;
+  width: 12rem;
+  background-color: #ffffff;
+  border-left: 2px solid #dedede;
+  border-right: 2px solid #dedede;
+  cursor: pointer;
+  & a {
+    text-decoration: none;
+    color: black;
+    font-size: 1.6rem;
+  }
+`;
