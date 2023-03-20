cb195fed1b6f2e8b22f28aee67c39c2ac2f2a4ac
minjekang <kangminje777@gmail.com>
2023-03-20 19:40:32 +0900

    add: 라우터 설정

---
+++ src/pages/Main/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import * as S from './style';
+const Main = () => {
+  return <div></div>;
+};
+
+export default Main;
---
+++ src/pages/Main/style.ts
@@ -0,0 +1,5 @@
+import styled from '@emotion/styled';
+
+export const Layout = styled.section`
+  width: 100vw;
+`;
