f95c01c13813efff47f5b37d7e26d22924573251
minjekang <kangminje777@gmail.com>
2023-03-20 20:33:46 +0900

    add: 게시물 만들기 버튼

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import Header from '../../components/common/Header';
 import * as S from './style';
 const Main = () => {
-  return <div></div>;
+  return (
+    <S.Layout>
+      <Header />
+      <S.Nav>
+        <S.NavLayout>
+          <S.MakeChoiceBtn>Choice만들기</S.MakeChoiceBtn>
+        </S.NavLayout>
+      </S.Nav>
+    </S.Layout>
+  );
 };
 
 export default Main;
--- src/pages/Main/style.ts
+++ src/pages/Main/style.ts
@@ -2,4 +2,27 @@ import styled from '@emotion/styled';
 
 export const Layout = styled.section`
   width: 100vw;
+  margin-top: 13rem;
+`;
+
+export const Nav = styled.nav`
+  width: 100vw;
+  display: flex;
+  justify-content: space-around;
+`;
+
+export const NavLayout = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 102rem;
+`;
+
+export const MakeChoiceBtn = styled.button`
+  font-size: 1.8rem;
+  background: #ffffff;
+  border: 2px solid #000000;
+  border-radius: 100px;
+  width: 22.8rem;
+  height: 5.8rem;
+  cursor: pointer;
 `;
