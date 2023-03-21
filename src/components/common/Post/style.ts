c48e2c46915aa4975f8632627744a879ec1d6f2d
minjekang <kangminje777@gmail.com>
2023-03-21 14:48:08 +0900

    add: Info box

--- src/components/common/Post/style.ts
+++ src/components/common/Post/style.ts
@@ -9,4 +9,47 @@ export const Post = styled.div`
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
+  position: relative;
+`;
+
+export const InfoBox = styled.section`
+  border-radius: 0 0 2rem 2rem;
+  width: 26rem;
+  height: 8rem;
+  background-color: #ffffff;
+  opacity: 0.9;
+  position: absolute;
+  bottom: 0;
+  padding: 1rem 2rem;
+  @font-face {
+    font-family: 'AppleSDGothicNeoUL';
+    font-style: normal;
+    src: url('/font/AppleSDGothicNeoUL.woff') format('woff');
+  }
+  h1 {
+    font-family: AppleSDGothicNeoUL;
+    font-size: 1.8rem;
+  }
+  p {
+    font-family: AppleSDGothicNeoUL;
+    font-size: 1.3rem;
+    color: #737373;
+    font-weight: 600;
+  }
+`;
+
+export const Info = styled.div`
+  display: flex;
+  position: absolute;
+  bottom: 1rem;
+  right: 0rem;
+`;
+
+export const Count = styled.p`
+  font-size: 1.5rem;
+  margin: auto 0;
+  font-weight: bold;
+  color: #000000 !important;
+  margin-right: 2rem;
+  margin-left: 3px;
 `;
