182b38659a1f9187b345aad4218d83c37c11f8a2
minjekang <kangminje777@gmail.com>
2023-03-22 23:19:30 +0900

    add: 게시물 상세 및 투표 퍼블리싱

--- src/pages/PostDetail/index.tsx
+++ src/pages/PostDetail/index.tsx
@@ -1,12 +1,44 @@
 import React from 'react';
 import Header from '../../components/common/Header';
+import Post from '../../components/common/Post';
 import * as S from './style';
 const PostDetail = () => {
   return (
     <>
       <Header />
       <S.Layout>
-        <div></div>
+        <S.PostDetailSection>
+          <h1>오늘 저녁 메뉴</h1>
+          <S.Detail>
+            <S.Description>
+              오늘 저녁 메뉴를 골라주세요! 썸녀랑 첫 데이트 나왔는데 뭐가 더
+              좋을까요?????? 오늘 저녁 골라줘
+            </S.Description>
+            <S.VoteBox>
+              <S.OptionBox>
+                <S.Option className='first'>
+                  <S.OptionName>
+                    <p>스테이크</p>
+                  </S.OptionName>
+                </S.Option>
+                <p>VS</p>
+                <S.Option className='second'>
+                  <S.OptionName>
+                    <p>스테이크</p>
+                  </S.OptionName>
+                </S.Option>
+              </S.OptionBox>
+              <S.ButtonWrap>
+                <S.VoteButton>
+                  <img src='svg/Check.svg' alt='' />
+                </S.VoteButton>
+                <S.VoteButton>
+                  <img src='svg/Check.svg' alt='' />
+                </S.VoteButton>
+              </S.ButtonWrap>
+            </S.VoteBox>
+          </S.Detail>
+        </S.PostDetailSection>
       </S.Layout>
     </>
   );
--- src/pages/PostDetail/style.ts
+++ src/pages/PostDetail/style.ts
@@ -1,10 +1,99 @@
 import styled from '@emotion/styled';
 
 export const Layout = styled.section`
-  width: 100vw;
-  margin-top: 17rem;
-  & div {
-    width: 76vw;
-    margin: 0 auto;
+  padding-top: 17rem;
+  width: 76vw;
+  margin: 0 auto;
+  display: flex;
+`;
+
+export const PostDetailSection = styled.section`
+  width: 100rem;
+  text-align: center;
+  & h1 {
+    font-size: 3rem;
+  }
+`;
+
+export const Detail = styled.section`
+  width: 100%;
+  border-top: 2px solid #f0f0f0;
+  border-bottom: 2px solid #f0f0f0;
+  margin: 3.5rem 0;
+  padding: 3.5rem 0;
+`;
+
+export const Description = styled.p`
+  margin: 0 auto;
+  width: 60rem;
+  font-size: 2rem;
+  line-height: 3.4rem;
+`;
+
+export const VoteBox = styled.div`
+  padding: 6rem 0 5rem 0;
+  width: 100%;
+  & p {
+    font-size: 3rem;
+    color: #6d6d6d;
+    font-weight: bold;
+    margin: auto;
+  }
+`;
+
+export const OptionBox = styled.div`
+  display: flex;
+  width: 74rem;
+  justify-content: space-between;
+  margin: 0 auto;
+`;
+
+export const Option = styled.div`
+  width: 30rem;
+  height: 30rem;
+  background-color: aliceblue;
+  border-radius: 2rem;
+  background-image: url(post.png);
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  position: relative;
+  margin-bottom: 2.8rem;
+  box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.15);
+`;
+
+export const OptionName = styled.div`
+  border-radius: 0 0 2rem 2rem;
+  width: 26rem;
+  height: 8rem;
+  background-color: #ffffff;
+  opacity: 0.9;
+  position: absolute;
+  bottom: 0;
+  padding: 1rem 2rem;
+  & p {
+    color: #000000 !important;
+    font-size: 2.4rem;
+    line-height: 8rem;
+  }
+`;
+export const ButtonWrap = styled.div`
+  display: flex;
+  width: 61rem;
+  margin: 0 auto;
+  justify-content: space-between;
+`;
+
+export const VoteButton = styled.button`
+  width: 17rem;
+  height: 11rem;
+  background-color: #f3f3f3;
+  border-radius: 20px;
+  border: none;
+  box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.15);
+  transition: 0.3s ease;
+  cursor: pointer;
+  &:hover {
+    box-shadow: 1px 2px 10px #9f9f9f;
   }
 `;
