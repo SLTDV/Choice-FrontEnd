818ddac020fb6b7838c58d619a3752addbec0564
minjekang <kangminje777@gmail.com>
2023-03-22 17:12:57 +0900

    add: layout

--- src/pages/PostDetail/index.tsx
+++ src/pages/PostDetail/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import Header from '../../components/common/Header';
 import * as S from './style';
 const PostDetail = () => {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <S.Layout>
+        <div></div>
+      </S.Layout>
+    </>
+  );
 };
 
 export default PostDetail;
--- src/pages/PostDetail/style.ts
+++ src/pages/PostDetail/style.ts
@@ -1,1 +1,10 @@
 import styled from '@emotion/styled';
+
+export const Layout = styled.section`
+  width: 100vw;
+  margin-top: 17rem;
+  & div {
+    width: 76vw;
+    margin: 0 auto;
+  }
+`;
