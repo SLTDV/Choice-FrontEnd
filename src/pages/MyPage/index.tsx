f82fd92f8ddaa1369d6f57ab8e9caf0229eda099
minjekang <kangminje777@gmail.com>
2023-03-26 21:47:22 +0900

    add: profile

--- src/pages/MyPage/index.tsx
+++ src/pages/MyPage/index.tsx
@@ -10,6 +10,9 @@ const MyPage = () => {
         <div>
           <S.ProfileBox>
             <img src='post.png' alt='profile image' />
+            <p>프로필 수정</p>
+            <input value='양세련' maxLength={6} minLength={2} />
+            <S.Edit />
           </S.ProfileBox>
         </div>
       </S.Layout>
--- src/pages/MyPage/style.ts
+++ src/pages/MyPage/style.ts
@@ -10,10 +10,45 @@ export const Layout = styled.section`
 export const ProfileBox = styled.section`
   text-align: center;
   width: 50rem;
+  position: relative;
   & img {
-    width: 25rem;
-    height: 25rem;
+    width: 26rem;
+    height: 26rem;
     border-radius: 25rem;
     object-fit: cover;
   }
+  & p {
+    color: #6d6d6d;
+    font-size: 1.5rem;
+    margin-top: 1.7rem;
+    text-decoration-line: underline;
+    cursor: pointer;
+  }
+  & input {
+    @font-face {
+      font-family: 'AppleSDGothicNeoUL';
+      font-style: normal;
+      src: url('/font/AppleSDGothicNeoUL.woff') format('woff');
+    }
+    font-family: AppleSDGothicNeoUL;
+    font-size: 2.5rem;
+    margin-top: 4rem;
+    border: none;
+    text-align: center;
+    border-bottom: 2px solid #f0f0f0;
+    outline: none;
+    font-weight: bold;
+    pointer-events: none;
+    width: 100%;
+    padding-bottom: 0.5rem;
+  }
+`;
+
+export const Edit = styled.div`
+  background-image: url('svg/Edit.svg');
+  width: 35px;
+  height: 35px;
+  position: absolute;
+  right: 0.8rem;
+  bottom: 0.8rem;
 `;
