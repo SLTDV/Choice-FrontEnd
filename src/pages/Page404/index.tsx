95ccd95a1aba50d0ac1a13b7e39ee596edc86945
minjekang <kangminje777@gmail.com>
2023-03-28 08:25:10 +0900

    add: Layout

--- src/pages/Page404/index.tsx
+++ src/pages/Page404/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
-
+import * as S from './style';
 const Page404 = () => {
-  return <div>404</div>;
+  return (
+    <S.Layout>
+      <img src='svg/404.svg' alt='404' />
+    </S.Layout>
+  );
 };
 
 export default Page404;
---
+++ src/pages/Page404/style.ts
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+export const Layout = styled.section`
+  width: 100vw;
+  height: 100vh;
+  text-align: center;
+  position: relative;
+  & img {
+    position: absolute;
+    left: 28.5%;
+    top: 10.5%;
+  }
+`;
