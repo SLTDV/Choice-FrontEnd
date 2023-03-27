a0e70a93cceaf44d963267f49815ec88444b3a94
minjekang <kangminje777@gmail.com>
2023-03-27 12:16:19 +0900

    add: title

--- src/pages/MakeChoice/index.tsx
+++ src/pages/MakeChoice/index.tsx
@@ -6,7 +6,13 @@ const MakeChoice = () => {
     <>
       <Header />
       <S.Layout>
-        <div></div>
+        <div>
+          <S.Title
+            placeholder='제목을 입력하세요 (2~16자)'
+            maxLength={16}
+            minLength={2}
+          />
+        </div>
       </S.Layout>
       ;
     </>
--- src/pages/MakeChoice/style.ts
+++ src/pages/MakeChoice/style.ts
@@ -5,4 +5,19 @@ export const Layout = styled.section`
   height: 200px;
   display: flex;
   justify-content: space-around;
+  margin-top: 20rem;
+`;
+
+export const Title = styled.input`
+  width: 84rem;
+  border: none;
+  border-bottom: 2px solid #f0f0f0;
+  outline: none;
+  font-size: 3rem;
+  text-align: center;
+  font-weight: bold;
+  padding-bottom: 2.8rem;
+  &::placeholder {
+    color: #939393;
+  }
 `;
