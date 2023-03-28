413ca4952509520350d600e74582f243dbcc080b
minjekang <kangminje777@gmail.com>
2023-03-28 11:48:15 +0900

    add: page 404

--- src/pages/Page404/index.tsx
+++ src/pages/Page404/index.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import * as S from './style';
 const Page404 = () => {
   return (
     <S.Layout>
-      <img src='svg/404.svg' alt='404' />
+      <div>
+        <img src='svg/404.svg' alt='404' />
+        <S.Content>
+          <img src='svg/SmallLogo.svg' alt='' />
+          <h1>404</h1>
+          <span>
+            <p>입력하신 주소를 다시 확인해주세요.</p>
+            <Link to='/'>Choice페이지→</Link>
+          </span>
+        </S.Content>
+      </div>
     </S.Layout>
   );
 };
--- src/pages/Page404/style.ts
+++ src/pages/Page404/style.ts
@@ -2,12 +2,51 @@ import styled from '@emotion/styled';
 
 export const Layout = styled.section`
   width: 100vw;
-  height: 100vh;
-  text-align: center;
-  position: relative;
+  display: flex;
+  justify-content: space-around;
   & img {
-    position: absolute;
-    left: 28.5%;
-    top: 10.5%;
+    margin-top: 10vh;
+  }
+`;
+
+export const Content = styled.div`
+  text-align: center;
+  position: absolute;
+  top: 13rem;
+  left: 39.2%;
+  @font-face {
+    font-family: 'AppleSDGothicNeoEB';
+    font-style: normal;
+    src: url('/font/AppleSDGothicNeoEB.woff') format('woff');
+  }
+  @font-face {
+    font-family: 'AppleSDGothicNeoEB';
+    font-style: normal;
+    src: url('/font/AppleSDGothicNeoEB.woff') format('woff');
+  }
+
+  & h1 {
+    font-family: AppleSDGothicNeoEB;
+    font-size: 14rem;
+    font-weight: bolder;
+    margin-top: 8rem;
+    margin-bottom: -1rem;
+  }
+  & span {
+    display: flex;
+  }
+  & span p {
+    font-size: 2rem;
+  }
+  & span a {
+    font-size: 2rem;
+    color: #ffffff;
+    background-color: #000000;
+    padding: 1px 8px;
+    border-radius: 5px;
+    text-decoration: underline 1px;
+    font-family: AppleSDGothicNeoUL;
+    margin-left: 1.4rem;
+    margin-top: -0.3rem;
   }
 `;
