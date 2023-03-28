4e46c2fe6f3efeb2546a4086aefbd8b01cf0068b
minjekang <kangminje777@gmail.com>
2023-03-28 12:12:09 +0900

    add: 반응형

--- src/pages/Page404/index.tsx
+++ src/pages/Page404/index.tsx
@@ -5,14 +5,13 @@ const Page404 = () => {
   return (
     <S.Layout>
       <div>
-        <img src='svg/404.svg' alt='404' />
         <S.Content>
           <img src='svg/SmallLogo.svg' alt='' />
           <h1>404</h1>
-          <span>
+          <div>
             <p>입력하신 주소를 다시 확인해주세요.</p>
             <Link to='/'>Choice페이지→</Link>
-          </span>
+          </div>
         </S.Content>
       </div>
     </S.Layout>
--- src/pages/Page404/style.ts
+++ src/pages/Page404/style.ts
@@ -1,30 +1,31 @@
 import styled from '@emotion/styled';
 
 export const Layout = styled.section`
+  background-image: url('404.png');
+  height: 100vh;
   width: 100vw;
+  background-position: center;
+  background-repeat: no-repeat;
   display: flex;
+  flex-direction: column;
   justify-content: space-around;
-  & img {
-    margin-top: 10vh;
-  }
 `;
 
 export const Content = styled.div`
+  margin: auto;
+  width: 43.1rem;
+  margin-bottom: 14rem;
   text-align: center;
-  position: absolute;
-  top: 13rem;
-  left: 39.2%;
   @font-face {
     font-family: 'AppleSDGothicNeoEB';
     font-style: normal;
     src: url('/font/AppleSDGothicNeoEB.woff') format('woff');
   }
   @font-face {
-    font-family: 'AppleSDGothicNeoEB';
+    font-family: 'AppleSDGothicNeoUL';
     font-style: normal;
-    src: url('/font/AppleSDGothicNeoEB.woff') format('woff');
+    src: url('/font/AppleSDGothicNeoUL.woff') format('woff');
   }
-
   & h1 {
     font-family: AppleSDGothicNeoEB;
     font-size: 14rem;
@@ -32,13 +33,13 @@ export const Content = styled.div`
     margin-top: 8rem;
     margin-bottom: -1rem;
   }
-  & span {
+  & div {
     display: flex;
   }
-  & span p {
+  & div p {
     font-size: 2rem;
   }
-  & span a {
+  & div a {
     font-size: 2rem;
     color: #ffffff;
     background-color: #000000;
