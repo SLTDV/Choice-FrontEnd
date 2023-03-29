d307d74f82619900da4074780f38ffe92da3a6c4
minjekang <kangminje777@gmail.com>
2023-03-29 20:51:21 +0900

    add: 헤더 유저 프로필 api

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { loggedAtom } from '../../../atoms';
 import * as S from './style';
 import { useRecoilState } from 'recoil';
+import User from '../../../services/User';
+import tokenService from '../../../utils/tokenService';
 
 const Header = () => {
   const [logged] = useRecoilState(loggedAtom);
+  const [nickname, setNickname] = useState();
+  const [profileImageUrl, setProfileImageUrl] = useState();
+  const getMiniProfile = async () => {
+    if (logged) {
+      try {
+        const res: any = await User.getMiniProfile();
+        setNickname(res.data.nickname);
+        setProfileImageUrl(res.data.image);
+      } catch (error: any) {
+        console.log(error);
+      }
+    }
+  };
 
+  useEffect(() => {
+    getMiniProfile();
+  }, []);
   return (
     <S.Header>
       <Link to='/'>
@@ -15,8 +33,8 @@ const Header = () => {
       {logged ? (
         <Link to='/my'>
           <S.ProfileWrap>
-            <p>강민제</p>
-            <img src='' alt='' />
+            <p>{nickname}</p>
+            <img src={profileImageUrl} alt='' />
           </S.ProfileWrap>
         </Link>
       ) : (
