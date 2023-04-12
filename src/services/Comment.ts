69f665bf11b6bccb34d531b73a8d416220623de3
minjekang <kangminje777@gmail.com>
2023-04-12 15:09:05 +0900

    add: 댓글 추가 api

---
+++ src/services/Comment.ts
@@ -0,0 +1,18 @@
+import { instance } from '../libs/instance';
+
+class Comment {
+  addComment(idx: number, content: string) {
+    try {
+      return instance({
+        method: 'POST',
+        url: process.env.REACT_APP_BASE_URL + '/comment/' + idx,
+        data: {
+          content: content,
+        },
+      });
+    } catch (error) {
+      return error;
+    }
+  }
+}
+export default new Comment();
